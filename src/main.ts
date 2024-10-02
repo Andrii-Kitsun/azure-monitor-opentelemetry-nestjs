2bf7a1406d5070d15ac290f52df81dccbbc462ae
Andrii Kitsun <136628789+Andrii-Kitsun@users.noreply.github.com>
2024-10-02 10:16:01 +0300

    Migrate from express to fastify (#1)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { ServerConfigInterface, ServerConfig } from './config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  const { serverPort } = app.get<ServerConfigInterface>(ServerConfig.KEY);
+
+  await app.listen(serverPort, '0.0.0.0');
 }
+
 bootstrap();
